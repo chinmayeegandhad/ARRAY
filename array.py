8343ecd59e67f863daf51d201dadcd8125a61d0d
Chinmayee <01fe24ica055@kletech.ac.in>
2025-11-27 00:27:31 +0530

    Modified array

--- array.py
+++ array.py
@@ -1,13 +1,16 @@
 def process_scores():
-    # Accept scores from user input (comma-separated)
     scores = list(map(int, input("Enter scores separated by commas: ").split(",")))
 
     total = sum(scores)
     average = total / len(scores)
+    maximum = max(scores)
+    minimum = min(scores)
 
     print("Scores:", scores)
     print("Total Score:", total)
     print("Average Score:", round(average, 2))
+    print("Maximum Score:", maximum)
+    print("Minimum Score:", minimum)
 
 
 if __name__ == "__main__":
